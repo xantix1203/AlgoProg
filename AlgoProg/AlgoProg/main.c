69eeb0f992805d2216904c562094d13d5c27f0ea
COTTEN <arnaudcottenpro@gmail.com>
2020-11-25 13:41:34 +0100

    projet

---
+++ AlgoProg/AlgoProg/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AlgoProg
+//
+//  Created by Arnaud Cotten on 25/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- Projet/main.c
+++ Projet/main.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+
+
+int main(){
+  int prob[9];
+  int prod[3];
+  int rangemax;
+  FILE* data;
+  data = fopen("data.txt", 'r');
+  fscanf(data, "%d", prob);
+  fscanf(data, "%d", prod);
+  fscanf(data, "%d", rangemax);
+  printf("%d", prob);
+  return 0;
+}
