a49cb129396d62fe7d93127741ff9fad31bf838e
COTTEN <arnaudcottenpro@gmail.com>
2020-12-08 12:32:37 +0100

    wind_init

--- Projet/main.c
+++ Projet/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define LEN 9
 void print_tab(float tab[LEN]);
+int init_wind(void);
+void next_day(float tab[LEN], int state);
 
 int main(){
   float prob[9];
@@ -14,11 +18,13 @@ int main(){
     for (int i = 0; i < 3; i++)
       fscanf(data, "%f", &prod[i]);
     fscanf(data, "%d", &rangemax);
-    print_tab(prod);
     print_tab(prob);
-    printf("%d", rangemax);
+    printf("%d\n", rangemax);
+    int wind = init_wind();
+    printf("%d\n", wind);
   return 0;
   }
+  return 0;
 }
 
 void print_tab(float tab[LEN]){
@@ -26,4 +32,14 @@ void print_tab(float tab[LEN]){
     for (int i = 0; i < (LEN - 1); i++)
         printf("%f,", tab[i]);
     printf("%f}\n", tab[LEN - 1]);
+}
+
+int init_wind(){
+  srand(time(NULL));
+  int wind = rand()%3;
+  return wind;
+}
+
+void next_day(float tab[LEN], int state){
+
 }
