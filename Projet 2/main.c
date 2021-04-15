f799ed53b08289a828cff940036396b6adf8eeab
COTTEN <xantix1203@gmail.com>
2021-04-15 19:01:42 +0200

    calculation of mean consumption

--- Projet 2/main.c
+++ Projet 2/main.c
@@ -38,9 +38,15 @@ typedef struct{
   Equipment *head;
 }HouseConfig;
 
+typedef struct {
+  unsigned int mean_consumption;
+  unsigned int installation_cost;
+  unsigned int r_o_n_duration; /* duration of return on investment*/
+}Result;
 
 
 void menu(HashTable* hash_power, HouseConfig* house_config);
+void menu_house_config(HouseConfig* house_config);
 
 void initialize_hash_table(HashTable* hash_tab);
 void load_hash_table(HashTable* hash_tab);
@@ -58,6 +64,10 @@ void disp_equipment(Equipment *equipment);
 void disp_house_config(HouseConfig* house_config);
 void save_house_config(HouseConfig* HouseConfig);
 
+void compute(HashTable* hash_power, HouseConfig* house_config);
+unsigned int calc_mean_consumption(HashTable* hash_power, HouseConfig* house_config);
+
+
 
 int main(){
 	HashTable hash_power;
@@ -72,36 +82,56 @@ int main(){
 
 void menu(HashTable* hash_power, HouseConfig* house_config){
   int choice;
-  printf("\n***************************** menu *****************************\n");
-  printf("1/load house \n2/add equipment\n3/supress equipment\n4/show equipments\n5/save config\n");
+  printf("\n***************************** Menu *****************************\n");
+  printf("1/configure house\n2/compute\n");
+  printf("****************************************************************\nchoice: ");
+  scanf("%d", &choice);
+  printf("\n\n");
+  switch (choice){
+    case 1 :
+      menu_house_config(house_config);
+      menu(hash_power, house_config);
+    case 2 :
+      compute(hash_power, house_config);
+  }
+}
+
+
+void menu_house_config(HouseConfig* house_config){
+  int choice;
+  printf("\n********************** House Configurator **********************\n");
+  printf("1/load house \n2/add equipment\n3/supress equipment\n4/show equipments\n5/save config\n6/main menu\n");
   printf("****************************************************************\nchoice: ");
   scanf("%d", &choice);
   printf("\n\n");
   switch (choice){
     case 1 :
       load_house_config(house_config);
-      menu(hash_power, house_config);
+      menu_house_config(house_config);
       break;
     case 2 :
       add_equipment(house_config, init_equipment());
       printf(">>> Equipment was sucessfully added to the house");
-      menu(hash_power, house_config);
+      menu_house_config(house_config);
       break;
     case 3 :
       supress_equipment(house_config);
-      menu(hash_power, house_config);
+      menu_house_config(house_config);
       break;
     case 4 :
       disp_house_config(house_config);
-      menu(hash_power, house_config);
+      menu_house_config(house_config);
       break;
     case 5 :
       save_house_config(house_config);
+      menu_house_config(house_config);
       break;
     case 6 :
       break;
   }
 }
+
+
 
 
 unsigned long get_hash_value(char *string){
@@ -166,6 +196,9 @@ void print_hash_table_characteristics(HashTable* hash_tab){
 	printf("Total number of elements: %u \n", hash_tab->nb_elements);
 	printf("****************************************************************\n");
 }
+
+
+
 
 Equipment* init_equipment(){
   char word[MAX_WORD_LENGHT];
@@ -277,4 +310,40 @@ void disp_house_config(HouseConfig* house_config){
     }
   }
   printf("****************************************************************\n");
+}
+
+
+
+
+void compute(HashTable* hash_power, HouseConfig* house_config){
+  Result* result = (Result*) malloc(sizeof(Result));
+  result->mean_consumption = calc_mean_consumption(hash_power, house_config);
+  printf("%u\n", result->mean_consumption);
+  FILE* file;
+  file = fopen("Data/data.txt", "r");
+  if (file != NULL){
+    char type[MAX_WORD_LENGHT];
+    unsigned int solar_panel_cost;
+    fscanf(file, "%s %u", type, &solar_panel_cost);
+    unsigned int peak_power;
+    fscanf(file, "%s %u", type, &peak_power);
+  }
+}
+
+
+unsigned int calc_mean_consumption(HashTable* hash_power, HouseConfig* house_config){
+  Equipment* current = house_config->head;
+  unsigned int result = 0;
+  unsigned int intermediate;
+  int power;
+  while (current != NULL){
+    power = hash_power->Elements[get_hash_value(current->word)]->power;
+    intermediate = power*(current->number);
+    if (strcmp(current->word, "chauffage") == 0)
+      result += (intermediate)*(house_config->house_surface);
+    else
+      result += intermediate;
+    current = current->next;
+  }
+  return result;
 }
