8495419bfc8949ce95fc86f7b880cf646e7bb221
COTTEN <xantix1203@gmail.com>
2021-04-19 00:23:06 +0200

    final commit

--- Projet 2/README.txt
+++ Projet 2/README.txt
@@ -48,16 +48,29 @@
   - power.csv
     Ce fichier contient une banque de données liées aux appareils électriques du
     quotidien. Il est écrit avec sur chaque ligne le nom d'un appareil
-    éléctrique suivi d'un espace et de la consommation de cette appareil en kW.
+    éléctrique suivi d'un espace et de la consommation de cette appareil en W.
+    Le cas du "chauffage" est particulier, le chiffre indiqué est la
+    consommation moyenne hebdomadaire par mètre carré d'un système de chauffage.
+    en Wh/m^2. (la valeur préenregistrée est issu d'une étude trouvée sur le web)
 * "house_config.txt"
-  - Ce fichier contient l'ensemble des données relatives à la configuration du
-    logement:
-    >> La surface expoitable en (m^2) dont la valeur doit être écrites après la
-       mention exploitable_surface.
-    >> La surface de l'habitation (en m^2) nécessaire au calcul de la facture de
-       chauffage.
-    >> La liste des équipements de la maison. Chaque équipement est écrit sur
-       une ligne avec le nom de l'équipement (correspondant à celui présent dans
-       le fichier "power.csv"), suivi du nombre d'unités présentes dans le
-       logement et du nombre d'heures de fonctionnement par semaine)
-  ********************************************************************************
+  Ce fichier contient l'ensemble des données relatives à la configuration du
+  logement:
+  - La surface expoitable en (m^2) dont la valeur doit être écrites après la
+     mention exploitable_surface.
+  - La surface de l'habitation (en m^2) nécessaire au calcul de la facture de
+     chauffage.
+  - La liste des équipements de la maison. Chaque équipement est écrit sur
+     une ligne avec le nom de l'équipement (correspondant à celui présent dans
+     le fichier "power.csv"), suivi du nombre d'unités présentes dans le
+     logement et du nombre d'heures de fonctionnement par semaine).
+     Le cas du chauffage est un cas particulier, si l'utilisateur souhaite avoir
+     une estimation globale pour le chauffage de l'ensemble de sa maison, il
+     doit rentrer dans le fichier "house_config.txt" le mot chauffage suivi de 1
+     (1 installation de chauffage pour toute la maison) et 1 (ce chiffre permet
+     d'obtenir l'estimation actuelle correcte même si le chauffage est allumé
+     plus d'une heure par semaine).
+* "save.txt"
+  Ce fichier stocke le résultat de la simulation, il est écrasé à chaque
+  nouvelle simulation.
+
+  ******************************************************************************
--- Projet 2/main_1.c
+++ Projet 2/main_1.c
@@ -85,7 +85,7 @@ void menu(HashTable* hash_power, HouseConfig* house_config){
   // Main menu
   int choice;
   printf("\n***************************** Menu *****************************\n");
-  printf("1/configurateur de logement\n2/simulation\n3/changement des prix\n");
+  printf("1/configure house\n2/compute\n3/changer les prix\n");
   printf("****************************************************************\nchoice: ");
   scanf("%d", &choice);
   printf("\n\n");
@@ -101,9 +101,9 @@ void menu(HashTable* hash_power, HouseConfig* house_config){
       menu(hash_power, house_config);
       break;
     case 3 :
-      printf("Donner le prix d'un panneau solaire: ");
+      printf("donner le prix d'un panneaux solaire");
       scanf("%d",&price);
-      printf("Donner le prix du kWh");
+      printf("donner le prix du kWh");
       scanf("%f",&price2);
       change_data(price, price2);
       break;
@@ -114,9 +114,9 @@ void menu(HashTable* hash_power, HouseConfig* house_config){
 void menu_house_config(HouseConfig* house_config){
   // House configuration menu
   int choice;
-  printf("\n********************** Configurateur de logement **********************\n");
-  printf("1/charger depuis le fichier \n2/ajouter un équipement\n3/supprimer un équipement\n4/afficher les équipements\n5/sauvegarder la configuration\n6/menu principal\n");
-  printf("****************************************************************\nchoix: ");
+  printf("\n********************** House Configurator **********************\n");
+  printf("1/load house \n2/add equipment\n3/supress equipment\n4/show equipments\n5/save config\n6/main menu\n");
+  printf("****************************************************************\nchoice: ");
   scanf("%d", &choice);
   printf("\n\n");
   switch (choice){
@@ -126,7 +126,7 @@ void menu_house_config(HouseConfig* house_config){
       break;
     case 2 :
       add_equipment(house_config, init_equipment());
-      printf(">>> L'équipement a été ajouté avec succès");
+      printf(">>> Equipment was sucessfully added to the house");
       menu_house_config(house_config);
       break;
     case 3 :
@@ -197,17 +197,17 @@ void load_hash_table(HashTable *hash_tab){
     }
   }
   else
-    printf("\nfichier power.txt introuvable\n");
+    printf("\nNo power.txt file\n");
   fclose(file);
 }
 
 
 void print_hash_table_characteristics(HashTable* hash_tab){
 	float loadFactor =  1.0*hash_tab->nb_occupied_entries/hash_tab->size;
-	printf("************** Caractéristiques de la table de hachage **********************\n");
-	printf("Nombre d'entrées: %u \n", hash_tab->size);
-	printf("Facteur de remplissage: %.2f \n", loadFactor);
-	printf("Nombre total d'éléments: %u \n", hash_tab->nb_elements);
+	printf("************** Hash Table Characteristics **********************\n");
+	printf("Number of buckets: %u \n", hash_tab->size);
+	printf("Load Factor: %.2f \n", loadFactor);
+	printf("Total number of elements: %u \n", hash_tab->nb_elements);
 	printf("****************************************************************\n");
 }
 
@@ -219,13 +219,13 @@ Equipment* init_equipment(){
   // allows the user to enter the specifications of a new equipment he wants to
   // add to his house
   char word[MAX_WORD_LENGHT];
-  printf("Entrer le nom de l'équipement: ");
+  printf("Enter the equipment's name: ");
   scanf("%s", word);
   unsigned int number;
-  printf("\nNombre d'unités dans le logement: ");
+  printf("\nNumber of units in the house: ");
   scanf("%u", &number);
   unsigned int hours_per_week;
-  printf("\nNombre d'heures d'utilisation par semaine: ");
+  printf("\nNumber of hours of use per week: ");
   scanf("%u", &hours_per_week);
   printf("\n");
   Equipment *equipment = (Equipment *) malloc(sizeof(Equipment));
@@ -245,7 +245,7 @@ void add_equipment(HouseConfig* house_config, Equipment* equipment){
 
 void get_word(char* word){
   // gets the name of the equipment to remove from the configuration
-  printf("Entrer le nom de l'équipement à supprimer: ");
+  printf("Enter the name of the equipment to supress: ");
   scanf("%s", word);
 }
 
@@ -259,13 +259,13 @@ void supress_element(char* word, HouseConfig *house_config){
     current = current->next;
   }
   if (current == NULL)
-    printf(">>> nom d'équipement invalide\n");
+    printf(">>> wrong equipment's name\n");
   else{
     if (prec == NULL)
       house_config->head = current->next;
     else
       prec->next = current->next;
-    printf(">>> l'équipement %s a été supprimé avec succés\n", word);
+    printf(">>> %s was successfully removed from the house\n", word);
   }
 }
 
@@ -294,11 +294,11 @@ void load_house_config(HouseConfig* house_config){
         equipment->hours_per_week = hours_per_week;
         add_equipment(house_config, equipment);
     }
-    printf(">>> fichier de configuration chargé avec succés\n");
+    printf(">>> house loaded\n");
     fclose(file);
   }
   else
-    printf(">>> fichier house_config.txt introuvable\n");
+    printf(">>> empty house_config.txt file\n");
 }
 
 void save_house_config(HouseConfig* house_config){
@@ -314,23 +314,23 @@ void save_house_config(HouseConfig* house_config){
     current = current->next;
   }
   fclose(file);
-  printf(">>> Configuration sauvegardée dans le fichier \"house_config.txt\"\n");
+  printf(">>> house configuration saved in \"house_config.txt\"\n");
 }
 
 void disp_equipment(Equipment *equipment){
   // displays an equipment
-  printf("équipement: %s, nombre d'unités: %u, heures d'utilisation par semaine: %u\n", equipment->word, equipment->number, equipment->hours_per_week);
+  printf("equipment: %s, number of units: %u, hours of use per week: %u\n", equipment->word, equipment->number, equipment->hours_per_week);
 }
 
 void disp_house_config(HouseConfig* house_config){
   // prints house_config
   Equipment *current = house_config->head;
-  printf("****************** Configuration du logement **************************\n");
-  printf("Surface du logement (m^2): %u\n", house_config->house_surface);
-  printf("Surface exploitable (m^2): %u\n", house_config->exploitable_surface);
-  printf(">>> Équipements:\n");
+  printf("****************** House's configuration **************************\n");
+  printf("House's surface: %u\n", house_config->house_surface);
+  printf("House's exploitable surface: %u\n", house_config->exploitable_surface);
+  printf(">>> Equipments:\n");
   if (current == NULL)
-    printf("Pas d'équipements à afficher\n");
+    printf("No equipment to show\n");
   else{
     while ((current != NULL)){
       disp_equipment(current);
@@ -346,10 +346,10 @@ void disp_house_config(HouseConfig* house_config){
 
 void compute(HashTable* hash_power, HouseConfig* house_config){
   // main simulation function
-  //calculation of the mean annual consumption of the house in kWh
+  //calculates the mean annual consumption of the house in kWh
   unsigned int mean_consumption = calc_mean_consumption(hash_power, house_config);
-  printf("Consommation moyenne: %u\n", mean_consumption);
-  //loading the price of a solar panel and of the kWh
+  printf("mean consumption: %u\n", mean_consumption);
+  //load the price of a solar panel and of the kWh
   FILE* file;
   file = fopen("Data/data.txt", "r");
   unsigned int solar_panel_cost;
@@ -360,36 +360,36 @@ void compute(HashTable* hash_power, HouseConfig* house_config){
     fscanf(file,"%s %f", type, &price_kwh);
   }
   fclose(file);
-  //returns the price of the consumption of the house
+  //return the price of the consumption of the house
   float price_consumption=(mean_consumption)*price_kwh;
-  printf("La consommation électrique de la maison revient à %f euros par an.\n", price_consumption);
-  //returns the surface necessary for the solar panel to produce the power needed by the house
+  printf("la consommation en électricité de la maison coute %f euros par ans\n", price_consumption);
+  //return the surface necessary for the solar panel to produce the power needed by the house
   unsigned int surface=surface_necessary(mean_consumption);
-  printf("Il faut utiliser une surface de %u m² pour compenser cette consommation.\n",surface);
-  printf("Combien de m² voulez vous utiliser pour l'installation de panneaux solaires ? \n");
+  printf("pour compenser sa consommation d'electricite il faut utiliser une surface de %u m² pour compenser sa consommation\n",surface);
+  printf("combien de m² voulez vous utiliser pour l'installation de panneaux solaire \n");
   scanf("%u",&(house_config->exploitable_surface));
-  //returns the number of solar panel needed
+  //return the number of solar panel needed
   int N=(house_config->exploitable_surface)/1.7; /*dimension of a solar panel : 1.7 m²*/
-  printf("On peut installer au maximum %d panneaux solaires sur cette surface.\n",N);
-  //returns the price of the installation
+  printf("On peut installer %d panneaux solaire au maximum sur cette surface\n",N);
+  //return the price of the installation
   unsigned int price=N*solar_panel_cost;
-  printf("Cette installation coûterait %u euros.\n",price);
-  //returns the production of the solar panel in kWh par an
+  printf("installer ces %d panneaux solaire coute %u euros\n",N,price);
+  //return the production of the solar panel in kWh par an
   float production_solar_panel=calc_kWc(N);
-  printf("Si les panneaux solaires sont produisent au maximum de leur capacité, ils génèreront %f kWh par an.\n",production_solar_panel);
+  printf("si les panneaux solaires sont optimisés ils produiront %f kW par an\n",production_solar_panel);
   float money_save=production_solar_panel*price_kwh;
-  //returns the money save by the installation
-  printf("Cela se traduira par une économie de %f euros par an\n",money_save);
-  //returns the time needed to refund the installation
+  //return the money save by the installation
+  printf("soit une economie de %f euros par an\n",money_save);
+  //return the time needed to refund the installation
   int time_refund=(price/money_save)+1;
-  printf("L'installation sera ainsi remboursée au cours de la %d année.\n",time_refund);
-  //saves the result in the file save.txt
+  printf("l'installation sera rembourse au cours de la %d année\n",time_refund);
+  //save the result in the file save.txt
   save_result(mean_consumption, price_consumption, surface, house_config->exploitable_surface, N, price, production_solar_panel, money_save, time_refund);
 }
 
 
 unsigned int calc_mean_consumption(HashTable* hash_power, HouseConfig* house_config){
-  // returns the mean annual consumption of the house in kWh.
+  // Calcultes the mean annual consumption of the house in kWh.
   Equipment* current = house_config->head;
   unsigned int result = 0;
   unsigned int intermediate;
@@ -408,33 +408,32 @@ unsigned int calc_mean_consumption(HashTable* hash_power, HouseConfig* house_con
 
 
 unsigned int surface_necessary(unsigned int consumption_house){
-  // returns the surface necessary to produce the consumption of the house with
-  // solar panel
-  FILE* file;
-  file=fopen(MONTHLY_DATA,"r");
-  if(file!=NULL){
-    char type[MAX_WORD_LENGHT];
-    char type2[MAX_WORD_LENGHT];
-    char type3[MAX_WORD_LENGHT];
-    char type4[MAX_WORD_LENGHT];
-    float passer;
-    fscanf(file,"%s %s %s %f",type, type2, type3, &passer);
-    fscanf(file,"%s %s %s %f",type, type2, type3, &passer);
-    fscanf(file,"%s %s %s",type, type2, type3);
-    fscanf(file,"%s %s %s  %s %f",type, type2, type3,type4, &passer);
-    fscanf(file,"%s %s %s ",type, type2, type3);
-    float kwc=0.0;
-    float production_month;
-    int year;
-    char month[MAX_WORD_LENGHT];
-    for(int i=0;i<12;i++){
-    	fscanf(file,"%d %s %f",&year, month , &production_month);
-    	kwc=kwc+production_month;
-    }
-    fclose(file);
-    unsigned int surface_necessary=consumption_house/kwc+2;
-    return surface_necessary;
-    }
+	// calculates the surface necessary to produce the consumption of the house with solar panel
+	 FILE* file;
+ 	 file=fopen(MONTHLY_DATA,"r");
+ 	 if(file!=NULL){
+  		char type[MAX_WORD_LENGHT];
+  		char type2[MAX_WORD_LENGHT];
+  		char type3[MAX_WORD_LENGHT];
+  		char type4[MAX_WORD_LENGHT];
+  		float passer;
+  		fscanf(file,"%s %s %s %f",type, type2, type3, &passer);
+  		fscanf(file,"%s %s %s %f",type, type2, type3, &passer);
+  		fscanf(file,"%s %s %s",type, type2, type3);
+  		fscanf(file,"%s %s %s  %s %f",type, type2, type3,type4, &passer);
+  		fscanf(file,"%s %s %s ",type, type2, type3);
+  		float kwc=0.0;
+  		float production_month;
+  		int year;
+  		char month[MAX_WORD_LENGHT];
+  		for(int i=0;i<12;i++){
+  			fscanf(file,"%d %s %f",&year, month , &production_month);
+  			kwc=kwc+production_month;
+  		}
+  		fclose(file);
+  		unsigned int surface_necessary=(consumption_house/kwc)+2;
+  		return surface_necessary;
+  }
   else{
   	fclose(file);
   	return 0;
@@ -444,7 +443,7 @@ unsigned int surface_necessary(unsigned int consumption_house){
 
 float calc_kWc(int N)
 {
-  //returns the power produced by N solar panel during a year
+	//calculate the power product by N solar panel during a year
   FILE* file;
   file=fopen(MONTHLY_DATA,"r");
   if(file!=NULL){
@@ -477,27 +476,27 @@ float calc_kWc(int N)
 
 
 void change_data(int price, float price2){
-  // updates the price of the solar panel and of the kWh
+	// change the price of the solar panel and of the kWh
 	FILE* file;
   	file = fopen("Data/data.txt", "w");
   	fprintf(file,"solar_panel_cost_per_kWc %d  \n",price);
   	fprintf(file,"price_kWh %f  ", price2);
-  	printf(">>> Les prix ont bien été changés");
+  	printf("les prix ont bien été changés");
  }
 
 
 void save_result(unsigned int consumption, float price_consumption, int surface, unsigned int exploitable_surface, int N, unsigned int price, float production_solar_panel, float money_save, int time_refund){
-  // saves the result of the simulation
-  FILE* file;
+ 	//save every important data about the simulation in the file save.txt
+	FILE* file;
  	file=fopen(SAUVEGARDE,"w");
- 	fprintf(file,"La consomation de la maison est %u kWh par an.\n",consumption);
- 	fprintf(file,"Cela coute %f par an.\n",price_consumption);
- 	fprintf(file,"Pour compenser cette consommation on a besoin de %u m² pour installer des panneaux solaires.\n",surface);
- 	fprintf(file,"Vous avez decider d'utiliser %u metres carrés pour votre installation.",exploitable_surface);
- 	fprintf(file,"Vous pouvez donc installer %d panneaux solaires pour %u euros.\n",N, price);
- 	fprintf(file,"Ces panneaux solaires vont produire %f kWh par an.\n",production_solar_panel);
- 	fprintf(file,"Ce qui se traduira par une économie de %f euros par an.\n",money_save);
- 	fprintf(file,"Il vous faudra donc %d années pour rentabiliser l'installation.",time_refund);
+ 	fprintf(file,"la consomation de la maison est %u kW par ans\n",consumption);
+ 	fprintf(file,"cela vous coutes %f\n",price_consumption);
+ 	fprintf(file,"pour compenser cette consommation on a besoin de %u m² pour installer des panneaux solaire\n",surface);
+ 	fprintf(file,"vous avez decider d'utiliser %u metres carré pour votre installation",exploitable_surface);
+ 	fprintf(file,"vous pouvez donc installer %d panneaux solairepour %u euros\n",N, price);
+ 	fprintf(file,"ces panneaux solaires vont produire %f kW par an\n",production_solar_panel);
+ 	fprintf(file,"cela va vous faire economiser %f euros par an",money_save);
+ 	fprintf(file,"il vous faudra donc %d années pour etre rembourser",time_refund);
  	fclose(file);
  }
 /******************************************************************************/
